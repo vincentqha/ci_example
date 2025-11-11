4b8a8193d1caf12887b0d340cb70af01d5c26689
Vincent Ha <vincentha@g.ucla.edu>
2025-11-10 22:47:38 -0800

    add test4

--- tests.py
+++ tests.py
@@ -15,6 +15,10 @@ class TestCase(unittest.TestCase):
     def test3(self):
         expected = "Hello Monkey"
         self.assertIsNot(task.my_func(), expected)
+    
+    def test4(self):
+        expected = "Hello Smurf"
+        self.assertIsNot(task.my_func(), expected)
 
 
 if __name__ == '__main__':
